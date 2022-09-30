ddd5a841fc968bb85f52b9ce5e1a6a0e11e3b3b1
carlpeters <cpjjpeters@gmail.com>
2022-09-30 12:08:23 +0200

    https://howtodoinjava.com/spring-webflux/webclient-get-post-example/

---
+++ src/main/java/be/tryout/config/AppConfig.java
@@ -0,0 +1,21 @@
+package be.tryout.config;
+
+/* carlpeters created on 30/09/2022 inside the package - be.tryout.config */
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class AppConfig
+{
+    @Bean
+    public static PropertyPlaceholderConfigurer getPropertyPlaceholderConfigurer()
+    {
+        PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+        ppc.setLocation(new ClassPathResource("application.properties"));
+        ppc.setIgnoreUnresolvablePlaceholders(true);
+        return ppc;
+    }
+}
