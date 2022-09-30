4cafd440d7cf0b0f3d55ad17031b937339d1769c
carlpeters <cpjjpeters@gmail.com>
2022-09-30 08:41:34 +0200

    Initial commit

---
+++ src/main/java/be/tryout/Application.java
@@ -0,0 +1,13 @@
+package be.tryout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/* carlpeters created on 12/09/2022 inside the package - PACKAGE_NAME */
+@SpringBootApplication
+public class Application
+{
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
